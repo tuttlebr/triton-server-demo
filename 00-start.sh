4c1e4b963750f7ef3b0bea668d9fc10187c62641
Brandon Tuttle <tuttlebr@ncc-1701.biz>
2023-09-11 11:35:09 -0400

    Update hooks
    Signed-off-by: Brandon Tuttle <tuttlebr@ncc-1701.biz>

--- 00-start.sh
+++ 00-start.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-
 clear
 echo "Uninstalling triton-demo..."
 helm uninstall triton-demo
 bash fetch_models.sh
-sudo docker compose build
-sudo docker compose push
+# docker compose build
+# docker compose push
 
 helm install --dependency-update triton-demo tritoninferenceserver/ \
     --set imageCredentials.password=${NGC_REGISTRY_API_KEY} \
     --set imageCredentials.email=${NGC_REGISTRY_EMAIL}
 
 sleep 10
+
+TRITON_POD=$(kubectl -n default get pod -l app=triton-inference-server -o name | grep client | cut -d \/ -f2 | sed -e 's/\\r$//g')
+echo "Copying models from host to PVC..."
+sleep 10
+kubectl cp model_repository ${TRITON_POD}:/opt/tritonserver/
 kubectl -n default get all -l app=triton-inference-server
